c9dbed4e458778712e47604e92f7c03db188dcc3
KevinBRitter <48842894+KevinBRitter@users.noreply.github.com>
2019-11-14 09:23:16 -0700

    morning push
    trying to catch up on slides

--- galifreyanwriter/bacs350/slides/index.php
+++ galifreyanwriter/bacs350/slides/index.php
@@ -3,9 +3,32 @@
     header("Pragma: no-cache");
     header("Expires: 0");
     header("Cache-Control: no-store, no-cache, must-revalidate");
+    
+    require_once 'log.php';
+    require_once 'slides.php';
+    require_once 'views.php';
 
+    // Log the page load
+    log_page();
 
+    // Display the page content
+    $content = '<div><p>';
+    $content .= render_button('Other Demos', '..');
+    $content .= render_button('Show Log', 'pagelog.php');
+    $content .= render_button('Add Slide Show', 'index.php?action=add');
+    $content .= '</p></div>';
+    $content .= handle_actions();
 
-    include (dirname(__DIR__).'/views.php');
+    // Create main part of page content
+    $settings = array(
+        "site_title" => "Slide Builder App",
+        "page_title" => "Working on reveal js",
+        "logo"       => "Bear.png",
+        "style"      => 'style.css',
+        "content"    => $content);
+
+    //include (dirname(__DIR__).'/views.php');
+
+    echo render_page($settings);
 
 ?>
